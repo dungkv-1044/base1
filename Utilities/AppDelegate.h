c630c54339e5c1ba7cef806bb80b4e411636dd54
khuat.van.dung <khuat.van.dung@framgias-macbook-pro-3.local>
2017-11-16 09:18:22 +0700

    Initial Commit

---
+++ Utilities/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Utilities
+//
+//  Created by khuat.van.dung on 11/16/17.
+//  Copyright © 2017 khuat.van.dung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Utilities/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Utilities
+//
+//  Created by khuat.van.dung on 11/16/17.
+//  Copyright © 2017 khuat.van.dung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
